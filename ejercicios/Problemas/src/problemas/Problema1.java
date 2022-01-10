772508acfc3597d4ae19f210fc382941ee6dff9a
Amon <you@example.com>
2022-01-09 22:34:39 -0500

    .......

--- ejercicios/Problemas/src/problemas/Problema1.java
+++ ejercicios/Problemas/src/problemas/Problema1.java
@@ -15,12 +15,24 @@ public class Problema1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int[][] informacion = {{1, 2, 3}, {10, 20, 30}, {100, 200, 300}};
+        int[][] informacion = { { 1, 2, 3 }, { 10, 20, 30 }, { 100, 200, 300 } };
         imprimirDatos(informacion);
     }
-    
-    public static void imprimirDatos(int[][] datos){
-    
+
+    public static void imprimirDatos(int[][] datos) {
+        String cadena = "";
+
+        for (int i = 0; i < datos.length; i++) {
+
+            for (int j = 0; j < datos[i].length; j++) {
+                int numero = datos[i][j];
+                if (numero % 2 == 0) {
+                    System.out.printf("%s\t", numero);
+
+                }
+
+            }
+        }
     }
-    
+
 }
